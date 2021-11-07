a03ef923f109ef5c951cfdfed99c16c7cfafcafd
Biswajit Dash <51367724+biswat@users.noreply.github.com>
2021-11-06 18:00:07 -1100

    changes in delete function

--- src/App.js
+++ src/App.js
@@ -15,10 +15,15 @@ export default function App(){
 
       function delTodo(event){
         var indexOfEvent = todo.indexOf(event.target.value);
-        todo.splice(indexOfEvent, 1);
+        var data = todo.splice(indexOfEvent, 1);
         // list.style.display = "none"
-        setTodo((oldTodo) => {
-          return [...oldTodo, todo];
+        var array = []
+        for(var i = 0; i < todo.length; i++) {
+            var value = [todo[i]]
+            array.push(value)
+          }
+        setTodo((oldTodo) => {  
+          return array;
         });
       }
 
